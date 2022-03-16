288e1c5547fdcacae8be92e1a447a0ccf829319f
kimhaerim <34bestsun@gmail.com>
2022-03-16 16:31:40 +0900

    7.회원 관리 예제

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -14,8 +15,13 @@ public class MemberController {
     @Autowired
     //의존관계 주입
     //memberService 스프링 컨테이너와 연결해줌
-    //스프링에서 memberService를 관리할 수 없어 MemberSerivce에 @Servic 붙여줘야 함
+    //스프링에서  memberService를 관리할 수 없어 MemberSerivce에 @Servic 붙여줘야 함
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
     }
 }
