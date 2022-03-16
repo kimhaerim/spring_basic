b1e61b83d4f33c17e170e547ade2c6684b219984
kimhaerim <34bestsun@gmail.com>
2022-03-16 14:04:26 +0900

    6. spring bean & DI

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,21 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    // 멤버 컨트롤러가 아닌 컨트롤러에서도 memberSerivce를 사용할 수 있다는 단점이 존재
+//    private final MemberService memberService = new MemberService();
+    private final MemberService memberService;
+
+    @Autowired
+    //의존관계 주입
+    //memberService 스프링 컨테이너와 연결해줌
+    //스프링에서 memberService를 관리할 수 없어 MemberSerivce에 @Servic 붙여줘야 함
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
